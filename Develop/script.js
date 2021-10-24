379d357fe1e2c0d71bd9c3aaf4da111d205d546f
brandon.sauer <brantheman8@comcast.net>
2021-10-24 09:50:04 -0700

    added javascript to ask user if they want to use numeric or special characters

--- Develop/script.js
+++ Develop/script.js
@@ -16,11 +16,33 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+//Ask user how many characters they want their password to be
 var characters = prompt("How many characters do you want your password to contain?");
+
 if (characters < 8) {
   alert("Password length must be between 8-128 characters");
 } if (characters > 128) {
   alert ("Password length must be between 8-128 characters");
 } if (characters > 8) {
   console.log (`I want ${characters} characters in my password`);
+}
+
+//Asks user if the want to use special characters
+var specialCharacter = true;
+var specialPrompt = prompt("Click OK if you would like to use special characters in your password.");
+
+if (specialPrompt === specialCharacter) {
+  console.log ("I would like special characters.");
+} else {
+  console.log ("I do NOT want to use special characters.");
+}
+
+//Ask user if they would like to use numeric characters
+var numericCharacter = true;
+var numericPrompt = prompt("Click OK to if you would like to use numeric characters in your password");
+
+if (numericPrompt === numericCharacter) {
+  console.log ("I want to use numeric characters.");
+} else {
+  console.log ("I do NOT want to use numeric characters");
 }
