bff975588db5f6b43868878173a45d078a266d92
brandon.sauer <brantheman8@comcast.net>
2021-10-24 09:34:33 -0700

    defining character parameters

--- Develop/script.js
+++ Develop/script.js
@@ -15,3 +15,12 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+var characters = prompt("How many characters do you want your password to contain?");
+if (characters < 8) {
+  alert("Password length must be between 8-128 characters");
+} if (characters > 128) {
+  alert ("Password length must be between 8-128 characters");
+} if (characters > 8) {
+  console.log (`I want ${characters} characters in my password`);
+}
