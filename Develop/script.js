0a21b9e7f5571a7d156d4be1012b39f913833115
brandon.sauer <brantheman8@comcast.net>
2021-10-24 12:30:55 -0700

    finished password generator code and added to README

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,22 @@
-// Assignment code here
-
-
-// Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
+var passwordLength;
+var confirmLower;
+var confirmUpper;
+var confirmNumber;
+var confirmSpecial;
+var userClick;
+
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r","s", "t", "u", "v", "w", "x", "y", "z"];
+
+var blankUpper = [];
+var toUpper = function (x) {
+  return x.toUpperCase();
+};
+upperCase = lowerCase.map(toUpper);
+//--------------------------------------
+var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+var special = ["!", "#", "$", "%", "&", "*", "@"];
 
 // Write password to the #password input
 function writePassword() {
@@ -16,58 +30,112 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-//Ask user how many characters they want their password to be
-var characters = prompt("How many characters do you want your password to contain?");
+// Start Function
+function generatePassword() {
+  // Ask four user Input
+  passwordLength = prompt("How many characters would you like your password? Choose between 8 and 128");
+  console.log("Password length " + passwordLength);
 
-if (characters < 8 || characters > 128) {
-  alert ("Password length must be between 8-128 characters");
-  prompt("How many characters would you like your password to be?");
-} else if (characters > 8 || characters < 128) {
-  console.log (`I want ${characters} characters in my password`);
+  if(!passwordLength) {
+    alert("Needs a value");
+
+  } else if (passwordLength < 8 || passwordLength > 128) {
+    passwordLength = prompt("You must choose between 8 and 128");
+    console.log("Password length " + passwordLength);
+
+  } else { 
+    confirmLower = confirm("Do you want lower case characters in your password?");
+    console.log("Lower case " + confirmLower);
+    confirmUpper = confirm("Do you want to use upper case characters in your password?");
+    console.log("Upper case " + confirmUpper);
+    confirmNumber = confirm("Do you want to use numbers in your password?");
+    console.log("Number " + confirmNumber);
+    confirmSpecial = confirm("Do you want to use special characters in your password?");
+    console.log("Special Character " + confirmSpecial);
+
+  };
+
+  // If the user clicks cancel 4 times
+  if (!confirmLower && !confirmUpper && !confirmNumber && !confirmSpecial) {
+    userClick = alert("You must choose an option");
+  // If user clicks OK 4 times.
+  } else if (confirmLower && confirmUpper && confirmNumber && confirmSpecial) {
+    userClick = lowerCase.concat(upperCase, numbers, special);
+    console.log(userClick);
+  }
+  // If user clicks OK 3 times
+  else if (confirmLower && confirmUpper && confirmNumber) {
+    userClick = lowerCase.concat(upperCase, numbers);
+    console.log(userClick);
+  }
+  else if (confirmLower && confirmUpper && confirmSpecial) {
+    userClick = lowerCase.concat(upperCase, special);
+    console.log(userClick);
+  }
+  else if (confirmLower && confirmNumber && confirmSpecial) {
+    userClick = lowerCase.concat(numbers, special);
+    console.log(userClick);
+  }
+  else if (confirmUpper && confirmNumber && confirmSpecial) {
+    userClick = upperCase.concat(numbers, special);
+    console.log(userClick);
+  }
+  // If user clicks OK 2 times
+  else if (confirmLower && confirmUpper) {
+    userClick = lowerCase.concat(upperCase);
+    console.log(userClick);
+  }
+  else if (confirmLower && confirmNumber) {
+    userClick = lowerCase.concat(numbers);
+    console.log(userClick);
+  }
+  else if (confirmLower && confirmSpecial) {
+    userClick = lowerCase.concat(special);
+    console.log(userClick);
+  }
+  else if (confirmUpper && confirmNumber) {
+    userClick = upperCase.concat(numbers);
+    console.log(userClick);
+  }
+  else if (confirmUpper && confirmSpecial) {
+    userClick = upperCase.concat(special);
+    console.log(userClick);
+  }
+  else if (confirmNumber && confirmSpecial) {
+    userClick = numbers.concat(special);
+    console.log(userClick);
+  }
+  // If user clicks OK only 1 time
+  else if (confirmLower) {
+    userClick = lowerCase;
+    console.log(userClick);
+  }
+  else if (confirmUpper) {
+    userClick = blankUpper.concat(upperCase);
+    console.log(userClick);
+  }
+  else if (confirmNumber) {
+    userClick = numbers;
+    console.log(userClick);
+  }
+  else if (confirmSpecial) {
+    userClick = special;
+    console.log(userClick);
+  };
+
+  //an empty variable for the password length. 
+  var passwordBlank = [];
+
+  // Creates a random selection
+  for (var i = 0; i < passwordLength; i++) {
+    var allClicks = userClick[Math.floor(Math.random() * userClick.length)];
+    passwordBlank.push(allClicks);
+    console.log(allClicks);
+  }
+
+  // Join and return the password 
+  var password = passwordBlank.join("");
+  console.log("Your Password is: " + password);
+  return password;
+
 }
-
-//Asks user if the want to use special characters
-var specialCharacter = true;
-var specialPrompt = confirm("Click OK if you would like to use special characters in your password.");
-
-if (specialPrompt === specialCharacter) {
-  console.log ("I would like special characters.");
-} else {
-  console.log ("I do NOT want to use special characters.");
-}
-
-//Ask user if they would like to use numeric characters
-var numericCharacter = true;
-var numericPrompt = confirm("Click OK to if you would like to use numeric characters in your password");
-
-if (numericPrompt === numericCharacter) {
-  console.log ("I want to use numeric characters.");
-} else {
-  console.log ("I do NOT want to use numeric characters");
-}
-
-//Ask user if they would like to use lower case characters
-
-var lowerCase = true;
-var lowerPrompt = confirm("Click OK if you would like to use lower case characters in your password");
-
-if (lowerPrompt === lowerCase) {
-  console.log ("I would like to use lowercase characters.");
-} else {
-  console.log ("I would NOT like to use lower case characters.");
-}
-
-//Ask user if they would like to use upper case characters
-var upperCase = true;
-var upperPrompt = ("Click OK if you would like to use upper case characters in your password.")
-
-if (upperPrompt === upperCase) {
-  console.log ("I would like to use uppercase characters.");
-} else {
-  console.log ("I would NOT like to use uppercase characters.");
-}
-
-//If none chosen, loop to ask the question again
-alert("You must choose at least one option.")
-
-console.log("I do NOT want numeric characters");
--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# Password Generator Starter Code
+This is a password generator that will ask you 5 different questions:
+
+1. Asks the user how many characters between 8 and 128 they would like to use for their password.
+
+2. Does the user want to use lower case letters?
+
+3. Does the user want to use upper case letters?
+
+4. Does the user want to use numbers?
+
+5. Does the user want to use special characters?
+
+It will then randomize each of the selectors and generate a random password. 
+
+Here is screen shot of what it will look like when the user chooses all of their options;
+
+https://i.imgur.com/k6zLDFX.png
+
